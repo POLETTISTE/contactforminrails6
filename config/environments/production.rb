241da826472f9ac3b2fcf01a8d43086415564086
STEPHANE POLETTI <polettistephane@msn.com>
2021-11-20 22:22:55 +0100

    change domain adress in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,7 +125,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'contactforminrails6.heroku.com', # UPDATE THIS VALUE WITH YOUR OWN APP
+    :domain         => 'contactformhowtodo.heroku.com', # UPDATE THIS VALUE WITH YOUR OWN APP
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
